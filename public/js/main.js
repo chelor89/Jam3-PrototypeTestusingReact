a926257c93028cf9fe1422b8eb67a13840e80f1a
chelorope <cmarcelorp@gmail.com>
2016-05-24 16:06:23 -0300

    new main.js generated

--- public/js/main.js
+++ public/js/main.js
@@ -22290,7 +22290,9 @@ var Twitter = React.createClass({
         this.renderTweets(JSON.parse(xhttp.responseText));
       }
     }.bind(this);
-    xhttp.open("GET", "http://" + window.location.hostname + ":8080", true);
+    //xhttp.open("GET", "http://" + window.location.hostname + ":8080", true);
+    xhttp.open("GET", "http://twitserve-63723.onmodulus.net/", true);
+
     xhttp.setRequestHeader("Access-Control-Allow-Origin", window.location.href);
     xhttp.send();
   },
