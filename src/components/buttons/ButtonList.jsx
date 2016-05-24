55535fb2bf0f4d454f10019b2c68d4468bc01e4d
chelorope <cmarcelorp@gmail.com>
2016-05-24 19:01:55 -0300

    hamburger menu added

--- src/components/buttons/ButtonList.jsx
+++ src/components/buttons/ButtonList.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var Button = require('./Button.jsx');
-var Hamburger = require('./Hamburger.jsx');
+var Menu = require('react-burger-menu').slide;
 
 
 var ButtonList = React.createClass({
@@ -8,23 +8,21 @@ var ButtonList = React.createClass({
     return this.props.handleClick.bind(null, name);
   },
   eachButton: function(item){
-    if (item.name != "Image") {
       if (this.props.size == "big"){
         return <Button key={item.id} ref={item.name} name={item.name}
         click={this.boundClick(item.name)}/>
       }else {
         return (
-          <a key={item.id} onClick={this.boundClick(item.name)}>{item.name}</a>
+          <a className="menu-item" onClick={this.boundClick(item.name)} key={item.id}>{item.name}</a>
         )
       }
-    }
   },
   render: function(){
-    var buttons = this.props.views.map(this.eachButton)
+    var buttons = this.props.views.slice(1).map(this.eachButton)
     if (this.props.size == "big"){
       return <div id='buttons'><div>{buttons}</div></div>
     }else {
-      return <Hamburger buttons={buttons} />
+      return <Menu>{buttons}</Menu>;
     }
   }
 });
